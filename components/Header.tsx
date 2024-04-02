df7991182022424c84bc62185727a55b0c441383
Onelio Viera <ojvwebdesign@gmail.com>
2024-04-01 20:22:32 -0600

    updated files

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+  
+  return ( 
+  <header>
+    <Link href='/'>
+      <Image
+      
+      />
+    </Link>
+  </header>
+
+  );
+  
+}
+
+export default Header
